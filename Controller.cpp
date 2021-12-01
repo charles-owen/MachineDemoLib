646323b38bbf358c5f96f60da5eeaadbd292c662
Charles B. Owen <cbowen@msu.edu>
2021-12-01 11:50:34 -0500

    Working on adding Controller to MachineDemo

--- Controller.cpp
+++ Controller.cpp
@@ -3,10 +3,15 @@
  * @author Charles Owen
  */
 
+#include "pch.h"
 #include "Controller.h"
+
+#include <wx/cmdline.h>
 
 Controller::Controller(MachineDemoMainFrame* mainFrame, MachineView* machineView, ControlPanel* controlPanel) :
     mMainFrame(mainFrame), mMachineView(machineView), mControlPanel(controlPanel)
 {
+    wxCmdLineParser parser(wxTheApp->argc, wxTheApp->argv);
+
 
 }
